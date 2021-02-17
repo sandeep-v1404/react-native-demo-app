762f8dddb671951569a9f0c18a51d6bd70c364af
sandeep-v1404 <valpasanisandeep@gmail.com>
2021-02-17 14:15:56 +0530

    Changes

---
+++ components/CarItem/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { ImageBackground, Text, View } from "react-native";
+import styles from "./styles";
+
+const CarItem = (props) => {
+  return (
+    <View style={styles.carContainer}>
+      <ImageBackground
+        source={require("../../assets/images/ModelX.jpeg")}
+        style={styles.image}
+      />
+      <View style={styles.titles}>
+        <Text style={styles.title}>Model S</Text>
+        <Text style={styles.subTitle}>Starting at $69,428</Text>
+      </View>
+    </View>
+  );
+};
+export default CarItem;
---
+++ components/CarItem/styles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  carContainer: {
+    width: "100%",
+    height: "100%",
+  },
+  titles: {
+    marginTop: "30%",
+    width: "100%",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "600",
+  },
+  subTitle: {
+    fontSize: 16,
+    color: "#5c5e62",
+  },
+  image: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+});
+export default styles;
